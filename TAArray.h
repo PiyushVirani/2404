2ceea9609f3529c8d41b34fdc6cc936f4a09fa56
PiyushVirani <146486681+PiyushVirani@users.noreply.github.com>
2025-04-05 11:31:20 -0400

    Update TAArray.h

--- TAArray.h
+++ TAArray.h
@@ -1,22 +1,27 @@
 #ifndef TAARRAY_H
 #define TAARRAY_H
 
+#include <iostream>
 #include "TextArea.h"
 #include "defs.h"
+using namespace std;
 
 class TAArray {
   public:
+    // constructor
     TAArray();
+    // destructor
     ~TAArray();
     bool add(TextArea* textArea);
     bool add(TextArea* textArea, int index);
-    TextArea* get(const std::string& id) const;
+    TextArea* get(const string& id) const;
     TextArea* get(int index) const;
-    TextArea* remove(const std::string& id);
+    TextArea* remove(const string& id);
     TextArea* remove(int index);
     int getSize() const;
-    void print() const;
+    void print();
 
+  // member variables
   private:
     TextArea** textAreas;
     int size;
